ff826ea19f04aad8eaca9b971079b5840e9caefa
Emmanuelprime <emmanuelprime2@gmail.com>
2023-11-09 01:35:19 +0100

    good morning

--- build/rain_arm_controller/cmake_install.cmake
+++ build/rain_arm_controller/cmake_install.cmake
@@ -43,6 +43,13 @@ if(NOT DEFINED CMAKE_OBJDUMP)
 endif()
 
 if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/rain_arm_controller" TYPE DIRECTORY FILES
+    "/home/emmanuel/rainbot_ws/src/rain_arm_controller/config"
+    "/home/emmanuel/rainbot_ws/src/rain_arm_controller/launch"
+    )
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
   file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ament_index/resource_index/package_run_dependencies" TYPE FILE FILES "/home/emmanuel/rainbot_ws/build/rain_arm_controller/ament_cmake_index/share/ament_index/resource_index/package_run_dependencies/rain_arm_controller")
 endif()
 
